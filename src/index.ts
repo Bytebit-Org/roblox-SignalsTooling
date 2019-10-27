272139d55aaafd2516dff83bfa6f065887f771e4
Noah Crowley <noahwillcrow@gmail.com>
2019-10-26 18:16:58 -0700

    Add ISignalConnection export

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 export * from "./Interfaces/IConnectionManager";
 export * from "./Interfaces/IReadOnlySignal";
 export * from "./Interfaces/ISignal";
+export * from "./Interfaces/ISignalConnection";
 
 // Classes
 export * from "./Implementation/ConnectionManager";
