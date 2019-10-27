7c3c196ee16c5fa036f261e9da17a25ee69472da
Noah Crowley <noahwillcrow@gmail.com>
2019-10-27 02:40:44 -0700

    Update generated files

--- out/init.lua
+++ out/init.lua
@@ -1,5 +1,5 @@
--- Compiled with https://roblox-ts.github.io v0.2.14
--- October 26, 2019, 9:56 PM Pacific Daylight Time
+-- Compiled with https://roblox-ts.github.io v0.2.15-commit-6424488.0
+-- October 27, 2019, 2:40 AM Pacific Daylight Time
 
 local TS = _G[script];
 local exports = {};
